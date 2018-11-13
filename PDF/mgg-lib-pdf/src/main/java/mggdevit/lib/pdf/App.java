68a224deabb7b2cc1c9f4f98ff781335a9ec1ffb
mgg-dev-it <44374657+mgg-dev-it@users.noreply.github.com>
2018-11-13 12:37:05 +0100

    .

---
+++ PDF/mgg-lib-pdf/src/main/java/mggdevit/lib/pdf/App.java
@@ -0,0 +1,13 @@
+package mggdevit.lib.pdf;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
