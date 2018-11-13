b1ebc566cb3d1eb7acc679d2440aafd80cc2ee19
mgg-dev-it <44374657+mgg-dev-it@users.noreply.github.com>
2018-11-13 13:59:50 +0100

    .

---
+++ PDF/mgg-lib-pdf/src/main/java/mggdevit/lib/pdf/icepdf/IcePdfUtil.java
@@ -0,0 +1,47 @@
+package mggdevit.lib.pdf.icepdf;
+
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.util.ArrayList;
+
+import org.icepdf.core.exceptions.PDFException;
+import org.icepdf.core.exceptions.PDFSecurityException;
+import org.icepdf.core.pobjects.Document;
+import org.icepdf.core.pobjects.Page;
+import org.icepdf.core.util.GraphicsRenderingHints;
+
+public abstract class IcePdfUtil {
+
+	public static ArrayList<BufferedImage> name(byte[] b) {
+		Document document = new Document();
+		ArrayList<BufferedImage> alBIs = new ArrayList<>();
+		try {
+			document.setByteArray(b, 0, b.length, null);
+			// save page captures to file.
+			float scale = 2.0f;
+			float rotation = 0f;
+
+			// Paint each pages content to an image and
+			// write the image to file
+			// BufferedImage image = null;
+			for (int i = 0; i < document.getNumberOfPages(); i++) {
+				try {
+					// BufferedImage image = (BufferedImage) document.getPageImage(i,
+					// GraphicsRenderingHints.PRINT, Page.BOUNDARY_CROPBOX, rotation, scale);
+					BufferedImage image = (BufferedImage) document.getPageImage(i, GraphicsRenderingHints.SCREEN,
+							Page.BOUNDARY_CROPBOX, rotation, scale);
+					alBIs.add(image);
+				} catch (InterruptedException e) {
+				}
+			}
+
+			document.dispose();
+		} catch (PDFException e) {
+		} catch (PDFSecurityException e) {
+		} catch (IOException e) {
+		}
+		return (alBIs);
+
+	}
+
+}
