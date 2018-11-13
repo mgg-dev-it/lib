b9290a37b04e0ee8d67c963d1de75ebcd190c1d0
mgg-dev-it <44374657+mgg-dev-it@users.noreply.github.com>
2018-11-13 15:45:03 +0100

    .

--- PDF/mgg-lib-pdf/src/main/java/mggdevit/lib/pdf/icepdf/IcePdfUtil.java
+++ PDF/mgg-lib-pdf/src/main/java/mggdevit/lib/pdf/icepdf/IcePdfUtil.java
@@ -12,12 +12,36 @@ import org.icepdf.core.util.GraphicsRenderingHints;
 
 public abstract class IcePdfUtil {
 
+	/**
+	 * Converts a PDF document (given as a bíte array) to images.
+	 * 
+	 * @param b byte array representing the PDF document
+	 * @return ArrayList of BufferedImages
+	 * 
+	 * @see <a href=
+	 *      "http://www.icesoft.org/wiki/display/PDF/Converting+PDF+Page+Renderings">http://www.icesoft.org/wiki/display/PDF/Converting+PDF+Page+Renderings</a>
+	 */
 	public static ArrayList<BufferedImage> convertPdfToImage(byte[] b) {
+		return (convertPdfToImage(b, 1f));
+	}
+
+	/**
+	 * Converts a PDF document (given as a bíte array) to images.
+	 * 
+	 * @param b     byte array representing the PDF document
+	 * @param fZoom zoom ratio - how big should be the image
+	 * @return ArrayList of BufferedImages
+	 * 
+	 * @see <a href=
+	 *      "http://www.icesoft.org/wiki/display/PDF/Converting+PDF+Page+Renderings">http://www.icesoft.org/wiki/display/PDF/Converting+PDF+Page+Renderings</a>
+	 * 
+	 */
+	public static ArrayList<BufferedImage> convertPdfToImage(byte[] b, float fZoom) {
 		Document document = new Document();
 		ArrayList<BufferedImage> alBIs = new ArrayList<>();
 		try {
 			document.setByteArray(b, 0, b.length, null);
-			float scale = 2.0f;
+			// float scale = 2.0f;
 			float rotation = 0f;
 
 			for (int i = 0; i < document.getNumberOfPages(); i++) {
@@ -25,7 +49,7 @@ public abstract class IcePdfUtil {
 					// BufferedImage image = (BufferedImage) document.getPageImage(i,
 					// GraphicsRenderingHints.PRINT, Page.BOUNDARY_CROPBOX, rotation, scale);
 					BufferedImage image = (BufferedImage) document.getPageImage(i, GraphicsRenderingHints.SCREEN,
-							Page.BOUNDARY_CROPBOX, rotation, scale);
+							Page.BOUNDARY_CROPBOX, rotation, fZoom);
 					alBIs.add(image);
 				} catch (InterruptedException e) {
 				}
